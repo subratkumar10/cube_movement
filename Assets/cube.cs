67298ad30981676e73c040050281a57b8fac8106
subratkumar10 <kumarsubratpradhan.10@gmail.com>
2020-07-26 00:03:05 +0530

    cube_movement

---
+++ Assets/cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Translate(0f, Input.GetAxis("Vertical") * Time.deltaTime, 0f) ;
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical"));
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 
+    }
+}
